85b82846870a01a17d2294940899c0040714aee8
OlhadoRibeira <thedarthrevan3@o2.pl>
2021-04-11 20:25:28 +0200

    halp

---
+++ dev_ws/build/my_urdf/share/my_urdf/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/ceneusz/dwornik_matysek/dev_ws/build/my_urdf"
---
+++ dev_ws/install/my_urdf/share/my_urdf/rviz.launch.py
@@ -0,0 +1,21 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+  use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+  rviz2_file_name = 'robot.rviz'
+  return LaunchDescription([
+      Node(
+          package='rviz2',
+          executable='rviz2',
+          name='my_rviz',
+          output='screen',
+          parameters=[{'use_sim_time': use_sim_time}],
+          arguments=['-d', os.path.join(get_package_share_directory('my_urdf'),rviz2_file_name)])
+     
+  ])
