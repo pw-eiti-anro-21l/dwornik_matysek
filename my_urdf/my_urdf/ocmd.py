b0913d00221ceaf2b59797a3bd1eed8e6dad4390
Kamil <matysekkamil@wp.pl>
2021-05-23 17:40:56 +0200

    ocmd

--- my_urdf/my_urdf/ocmd.py
+++ my_urdf/my_urdf/ocmd.py
@@ -13,40 +13,42 @@ class Ocmd(Node):
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
-        self.elipsa=1
+        self.elipsa=0
 
 
     def timer_callback(self):
         msg = PoseStamped()
+        l=80
         if self.elipsa:
             a=1
             b=2
-            if self.i<40:
+            if self.i<l/2:
                 x=b-self.i*b/20
                 msg.pose.position.x = x
                 msg.pose.position.y = (1-x*x/(b*b))*a*a
-            elif self.i<80:
+            else:
                 x=-b+(self.i-40)*b/20
                 msg.pose.position.x = x
                 msg.pose.position.y = -(1-x*x/(b*b))*a*a
         else:
-            if self.i<10:
-                msg.pose.position.x = float(2)
-                msg.pose.position.y = float(self.i*1/5)
-            elif self.i<30:
-                msg.pose.position.x = float(2-(self.i-10)*2/10)
-                msg.pose.position.y = float(2)
-            elif self.i<50:
-                msg.pose.position.x = float(-2)
-                msg.pose.position.y = float(2-(self.i-30)*2/10)
-            elif self.i<70:
-                msg.pose.position.x = float(-2+(self.i-50)*2/10)
-                msg.pose.position.y = float(-2)
-            elif self.i<80:
-                msg.pose.position.x = float(2)
-                msg.pose.position.y = float(-2+(self.i-70)*1/5)
+            a=1.5
+            if self.i<(l/8):
+                msg.pose.position.x = float(a)
+                msg.pose.position.y = float(self.i*a*8/l)
+            elif self.i<(3*l/8):
+                msg.pose.position.x = float(a-(self.i-l/8)*a*8/l)
+                msg.pose.position.y = float(a)
+            elif self.i<(5*l/8):
+                msg.pose.position.x = float(-a)
+                msg.pose.position.y = float(a-(self.i-3*l/8)*a*8/l)
+            elif self.i<(7*l/8):
+                msg.pose.position.x = float(-a+(self.i-5*l/8)*a*8/l)
+                msg.pose.position.y = float(-a)
+            else:
+                msg.pose.position.x = float(a)
+                msg.pose.position.y = float(-a+(self.i-7*l/8)*a*8/l)
         msg.pose.position.z = float(0.1)
-        if self.i==79:
+        if self.i==(l-1):
             self.i=-1
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.pose.position)
